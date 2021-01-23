cd7a2b14f98ae4dccc549af015bd6de4fd4bc396
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-01-23 15:13:45 -0600

    UV Index Printing

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,39 @@
 var apiKey = "9cd194d163b26eabb565a48c6fa01c91"
 
+// Function to get and print UV Index
+function getUvIndex (lat, long) {
+
+    //Dollar sign in url = template literals. Back ticks, dollar sign, curly bracket
+    //Blake also said something about keeping functions available for page refresh?
+        var uvURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${long}&appid=${apiKey}`;
+    
+        $.ajax({
+            method: "GET",
+            url: uvURL
+        }).then(function(uvResponse){
+            console.log(uvResponse)
+            var uvItem = $("<p class='card-text'>");
+            uvItem.html("UV Index: ");
+            var numSpan = $("<span class='badge'>");
+            numSpan.html(uvResponse.value);
+            uvItem.append(numSpan);
+
+            if (uvResponse.value <= 5.99) {
+                numSpan.attr("style", "background-color: goldenrod;")
+            } else if (uvResponse.value > 5.99 && uvResponse.value < 8) {
+                numSpan.attr("style", "background-color: orange;")
+            } else if (uvResponse.value > 7.99 && uvResponse.value < 10.99) {
+                numSpan.attr("style", "background-color: red;")
+            } else {
+                numSpan.attr("style", "background-color: violet;")
+            }
+
+           
+            $(".card-body").append(uvItem);
+        })
+    }
+    
+
 $("#cityBtn").on("click", function () {
     $(".card-text").empty()
 
@@ -54,8 +88,19 @@ $("#cityBtn").on("click", function () {
         windItem.html("Wind Speed: " + windSpeed + " MPH");
         $(".card-body").append(windItem);
 
+        //Print UV Index
+        var lat = response.coord.lat;
+        // lat = lat.toFixed(2);
+        var long = response.coord.lon;
+        // long= long.toFixed(2);
+        console.log(lat, long);
 
+        getUvIndex(lat, long);
+        
 
     })
 
+    
+
 })
+
