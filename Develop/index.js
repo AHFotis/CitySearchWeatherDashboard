062c516d3bcebc35847b4830c779772f27a717bf
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-01-25 10:35:38 -0600

    Error catch functional

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,17 @@
 var apiKey = "9cd194d163b26eabb565a48c6fa01c91"
 
+//Renders the last dispalyed city from local storage
 function renderLast() {
-
     var location = localStorage.getItem("LastCity");
     if (location !== null) {
         $(".master").attr("style", "display; visible")
-        printAll(location);
+        printButtonInfo(location);
         renderLastButtons();
     }
 }
 renderLast();
 
+//Renders list of previous buttons from local storage
 function renderLastButtons() {
     var list = JSON.parse(localStorage.getItem("Cities"));
     if (list !== null) {
@@ -27,14 +28,14 @@ function printSave(place) {
     $(".list-group").append(newBtn);
 }
 
-//Turn epoch into date and print it to page
+//Turn epoch into date
 function getDate(object) {
     var epoch = moment.unix(object.dt);
     var date = epoch.format("(M/DD/YY)")
     return date;
 }
 
-//Get retrieve icon url
+//Retrieve icon url
 function printIcon(object) {
     var iconcode = object.weather[0].icon;
     var iconURL = "http://openweathermap.org/img/wn/" + iconcode + ".png";
@@ -68,7 +69,6 @@ function getUvIndex(object) {
         method: "GET",
         url: uvURL
     }).then(function (uvResponse) {
-        console.log(uvResponse)
         var uvItem = $("<p class='card-text'>");
         uvItem.html("UV Index: ");
         var numSpan = $("<span class='badge'>");
@@ -103,11 +103,9 @@ function fiveDay(object) {
         url: currentURL
     }).then(function (fiveDayResponse) {
         var forecast = (fiveDayResponse.daily)
-        console.log(forecast)
 
         for (var i = 1; i < 6; i++) {
             var day = forecast[i];
-            console.log(day);
             //create variable
             var dayDiv = $("<div class='card bg-dark p-1 forecast'>")
 
@@ -143,14 +141,17 @@ function fiveDay(object) {
 
 }
 
+//Function to render city when first submitted and save to local storage.
 function printAll(city) {
     var currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     $.ajax({
         method: "GET",
-        url: currentURL
-    }).then(function (response) {
-        console.log(response);
+        url: currentURL,
+        success: function (response) {
+
+        $(".master").attr("style", "display; visible")
+        $(".card-text").empty()
 
         //Retrieve date and format it
         var thisDate = getDate(response);
@@ -172,7 +173,6 @@ function printAll(city) {
         $(".mainBody").append(humItem);
 
         //Print wind speed
-
         var windSpeed = getWindSpeed(response);
         var windItem = $("<p class='card-text'>");
         windItem.html("Wind Speed: " + windSpeed + " MPH");
@@ -183,13 +183,71 @@ function printAll(city) {
 
         //Print 5 day
         fiveDay(response);
+
+        printSave(city);
+        localStorage.setItem("LastCity", city);
+        listStorage(city);
+        },
+
+        error: function (jqXHR) {
+            if (jqXHR.statusText == "Not Found") {
+                alert("Please type in a valid city.");
+            } else if (jqXHR.statusText == "Bad Request") {
+                alert("Please type in a city to get started!")
+            }
+
+            console.log(jqXHR);
+        }
     })
+}
+
+//Function to reprint info from button
+function printButtonInfo (city) {
+    var currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    $.ajax({
+        method: "GET",
+        url: currentURL,
+    }).then(function (buttonResponse) {
+
+        $(".master").attr("style", "display; visible")
+
+        //Retrieve date and format it
+        var thisDate = getDate(buttonResponse);
+        $(".mainTitle").text(buttonResponse.name + " " + thisDate);
+
+        //Print Icon
+        var icon = printIcon(buttonResponse);
+        $(".icon").attr("src", icon);
+
+        //Print temperature
+        var farPrint = getTemp(buttonResponse);
+        var tempItem = $("<p class='card-text'>");
+        tempItem.html("Temperature: " + farPrint + "&deg F");
+        $(".mainBody").append(tempItem);
+
+        //Print Humidity
+        var humItem = $("<p class='card-text'>");
+        humItem.html("Humidity: " + buttonResponse.main.humidity + "%");
+        $(".mainBody").append(humItem);
+
+        //Print wind speed
+        var windSpeed = getWindSpeed(buttonResponse);
+        var windItem = $("<p class='card-text'>");
+        windItem.html("Wind Speed: " + windSpeed + " MPH");
+        $(".mainBody").append(windItem);
+
+        //Print UV Index
+        getUvIndex(buttonResponse);
+
+        //Print 5 day
+        fiveDay(buttonResponse);
+})
 }
 
 //Function to save recent list of searches
 function listStorage(city) {
     var cities = JSON.parse(localStorage.getItem("Cities"));
-    console.log(cities)
 
     var openArray = [];
 
@@ -200,37 +258,24 @@ function listStorage(city) {
         cities.push(city)
         localStorage.setItem("Cities", JSON.stringify(cities));
     }
-
-
 }
-
 
 //on click event to print search field input to page
 $("#cityBtn").on("click", function () {
-    $(".card-text").empty()
-    $(".master").attr("style", "display; visible")
 
     var city = $("#inputDefault").val().trim();
-    console.log(city);
     $("#inputDefault").val("");
 
-
     printAll(city);
-    printSave(city);
-
-    localStorage.setItem("LastCity", city);
-    listStorage(city);
 })
 
 //Document helps when you are appending new classes that might not live on the page at first
 $(document).on("click", ".saveCity", function () {
-    console.log("hello");
     $(".card-text").empty()
 
     var newCity = $(this).text();
-    console.log(newCity);
 
-    printAll(newCity);
+    printButtonInfo(newCity);
 
     localStorage.setItem("LastCity", newCity);
 })
